66aa6f52bdd615e176f73f4256b591c4ad284f60
ynubhav <anubhavdixit35@gmail.com>
2026-02-24 12:56:15 +0530

    feat: enhance Providers and Sidebar components with Toaster and logout confirmation

--- components/Providers.tsx
+++ components/Providers.tsx
@@ -1,11 +1,13 @@
 "use client";
 
 import { SessionProvider } from "next-auth/react";
+import { Toaster } from "sonner";
 
-export default function Providers({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  return <SessionProvider>{children}</SessionProvider>;
+export default function Providers({ children }: { children: React.ReactNode }) {
+  return (
+    <SessionProvider>
+      <Toaster position="top-center" />
+        {children}
+    </SessionProvider>
+  );
 }
--- components/dashboard/Sidebar.tsx
+++ components/dashboard/Sidebar.tsx
@@ -10,28 +10,32 @@ import {
   History,
   LogOut,
 } from "lucide-react";
+import { toast } from "sonner";
 
 export default function Sidebar({ user }: { user: string }) {
   const pathname = usePathname();
 
   const linkStyle = (path: string) =>
     `flex items-center gap-3 px-4 py-3 rounded-lg transition-all duration-200
-     ${pathname === path
-        ? "bg-indigo-500 text-white"
-        : "text-slate-400 hover:bg-slate-800"
-      }`;
+     ${
+       pathname === path
+         ? "bg-indigo-500 text-white"
+         : "text-slate-400 hover:bg-slate-800"
+     }`;
 
   return (
     <aside
       className="
-        group h-screen bg-slate-950 border-r border-slate-800
-        w-20 hover:w-64 transition-all duration-300
-        flex flex-col justify-between
-      "
+    group fixed top-0 left-0 z-40
+    h-screen
+    w-16 md:w-20
+    bg-slate-950 border-r border-slate-800
+    transition-all duration-300
+    hover:w-64
+  "
     >
       {/* TOP */}
       <div className="p-4">
-
         {/* Logo */}
         <div className="flex items-center justify-center group-hover:justify-start gap-3 mb-10 transition-all">
           <LayoutDashboard className="w-6 h-6 text-indigo-500 shrink-0" />
@@ -42,7 +46,6 @@ export default function Sidebar({ user }: { user: string }) {
 
         {/* Nav */}
         <nav className="space-y-3">
-
           <NavItem
             href="/dashboard"
             label="Home"
@@ -70,21 +73,24 @@ export default function Sidebar({ user }: { user: string }) {
             Icon={History}
             pathname={pathname}
           />
-
         </nav>
       </div>
 
       {/* BOTTOM */}
       <div className="p-4 border-t border-slate-800">
-
         <div className="mb-4 hidden group-hover:block">
           <p className="text-xs text-slate-500">Logged in as</p>
           <p className="text-sm font-medium truncate">{user}</p>
         </div>
 
         <button
-          onClick={() => signOut()}
-          className="flex items-center justify-center group-hover:justify-start gap-3 w-full px-4 py-3 rounded-lg bg-indigo-500 hover:bg-indigo-600 transition"
+          onClick={() => {
+            toast("Confirm Logout", {
+              action: {label: "Logout", onClick: () => signOut()},
+              cancel: {label: "Cancel", onClick: () => toast.dismiss() },
+            });
+          }}
+          className="flex items-center justify-center group-hover:justify-start gap-3 w-full px-4 py-3 rounded-lg bg-indigo-500 hover:bg-indigo-600 transition hover:cursor-pointer"
         >
           <LogOut className="w-5 h-5 shrink-0" />
           <span className="hidden group-hover:inline whitespace-nowrap">
@@ -114,10 +120,11 @@ function NavItem({
       href={href}
       className={`
         flex items-center justify-center group-hover:justify-start
-        gap-3 px-4 py-3 rounded-lg transition-all duration-200
-        ${active
-          ? "bg-indigo-500 text-white"
-          : "text-slate-400 hover:bg-slate-800"
+        gap-3 px-4 py-3 rounded-lg transition-all duration-200 hover:cursor-pointer
+        ${
+          active
+            ? "bg-indigo-500 text-white"
+            : "text-slate-400 hover:bg-slate-800"
         }
       `}
     >
