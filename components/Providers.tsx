fec7d3e5e5ed9e4675d528511bf346bd55a205a4
ynubhav <anubhavdixit35@gmail.com>
2026-02-25 21:49:50 +0530

    feat: enhance Toaster component with custom theme and options

--- components/Providers.tsx
+++ components/Providers.tsx
@@ -6,7 +6,7 @@ import { Toaster } from "sonner";
 export default function Providers({ children }: { children: React.ReactNode }) {
   return (
     <SessionProvider>
-      <Toaster position="top-center" />
+      <Toaster position="top-center" theme="dark" gap={12} toastOptions={{style:{backgroundColor:"midnightblue"}}}/>
         {children}
     </SessionProvider>
   );
